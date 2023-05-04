6a8008fea4d72469b0b5eb69258d9dfaef1e782e
Bithi90 <bka3670@gmail.com>
2023-05-05 05:02:39 +0600

    second

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ app.get('/chefData',(req,res) =>{
     res.send(chefData);
 })
 
+app.get('/chefData/:id', (req,res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedChefData = chefData.find(n=> n.id===id);
+    res.send(selectedChefData)
+})
+
+app.get('/categories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if(id === 0){
+        res.send(chefData)
+    }
+    else{
+        const categoryData = chefData.filter(c=> parseInt(c.category_id)===id);
+        res.send(categoryData) 
+    }
+   
+})
+
 app.listen(port, () => {
-    console.log(`Dragon API is running on port:${port}`)
+    console.log(`Recipe API is running on port:${port}`)
 })
